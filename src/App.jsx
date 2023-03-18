f3b7db24aed6e0a8f62355c223750a47fee22951
Ling <2845740149qq.com>
2023-03-18 23:30:42 +0800

    feat: style for sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 import Header from './components/Header';
+import SideBar from './components/SideBar';
 
 function App() {
 	return (
 		<Router>
-			<Switch>
-				<Route path="/" exact>
-					<Header />
-				</Route>
-			</Switch>
+			<Header />
+			<AppBody>
+				<SideBar />
+				<Switch>
+					<Route path="/" exact></Route>
+				</Switch>
+			</AppBody>
 		</Router>
 	);
 }
 
 export default App;
+
+const AppBody = styled.div`
+	display: flex;
+	height: 100vh;
+`;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+	Add,
+	Apps,
+	BookmarkBorder,
+	Drafts,
+	ExpandLess,
+	ExpandMore,
+	FiberManualRecord,
+	FileCopy,
+	Inbox,
+	InsertComment,
+	PeopleAlt,
+	Create,
+} from '@mui/icons-material';
+import SideBarOption from './SideBarOption';
+
+const SideBar = () => {
+	return (
+		<SideBarWrapper>
+			<SideBarHeader>
+				<SideBarInfo>
+					<h2>PAPA FAM HQ</h2>
+					<h3>
+						<FiberManualRecord />
+						Torrid
+					</h3>
+				</SideBarInfo>
+				<Create />
+			</SideBarHeader>
+			<SideBarOption Icon={InsertComment} title="Threads" />
+			<SideBarOption Icon={Inbox} title="Mentions & Reactions" />
+			<SideBarOption Icon={Drafts} title="Save items" />
+			<SideBarOption Icon={BookmarkBorder} title="Channel browser" />
+			<SideBarOption Icon={PeopleAlt} title="People & user groups" />
+			<SideBarOption Icon={Apps} title="Apps" />
+			<SideBarOption Icon={FileCopy} title="File browser" />
+			<SideBarOption Icon={ExpandLess} title="Show less" />
+			<hr />
+			<SideBarOption Icon={ExpandMore} title="Channels" />
+			<hr />
+			<SideBarOption Icon={Add} addChannelOption title="Add Channel" />
+		</SideBarWrapper>
+	);
+};
+
+export default SideBar;
+
+const SideBarWrapper = styled.div`
+	background-color: var(--slack-color);
+	flex: 3;
+	color: white;
+	border: 1px solid #49274b;
+	max-width: 260px;
+	margin-top: 60px;
+
+	> hr {
+		margin-top: 10px;
+		margin-bottom: 10px;
+		border: 1px solid #49274b;
+	}
+`;
+const SideBarHeader = styled.div`
+	display: flex;
+	border-bottom: 1px solid #49274b;
+	padding-bottom: 10px;
+	padding: 13px;
+  align-items: center;
+
+	> .MuiSvgIcon-root {
+		padding: 8px;
+		color: #49274b;
+		font-size: 18px;
+		background-color: white;
+		border-radius: 999px;
+	}
+`;
+const SideBarInfo = styled.div`
+	flex: 1;
+
+	> h2 {
+		font-size: 15px;
+		font-weight: 900px;
+		margin-bottom: 5px;
+	}
+
+	> h3 {
+		display: flex;
+		font-size: 13px;
+		font-weight: 400px;
+		align-items: center;
+	}
+
+	> h3 > .MuiSvgIcon-root {
+		font-size: 14px;
+		margin-top: 1px;
+		margin-right: 2px;
+		color: green;
+	}
+`;
