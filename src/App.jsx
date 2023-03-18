fc9e607161997c7ae86cf5c70b4eb29b99dafa8f
Ling <2845740149qq.com>
2023-03-18 21:54:01 +0800

    feat: setup project route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <h1>Hello</h1>
-  )
+	return (
+		<Router>
+			<Switch>
+				<Route path="/" exact>
+					<Header />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
-export default App
+export default App;
